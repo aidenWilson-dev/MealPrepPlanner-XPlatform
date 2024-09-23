3989df2a064dfe7f5df78903a05ad3500feef4e3
Aiden Wilson <aiden_dw@icloud.com>
2024-09-23 14:34:37 +1000

    Initial Commit

---
+++ MealPrepPlanner-XPlatform/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MealPrepPlanner_XPlatform;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ MealPrepPlanner-XPlatform/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MealPrepPlanner_XPlatform;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
