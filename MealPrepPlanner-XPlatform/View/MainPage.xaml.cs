b09c9016d91196b1e993f57299010bc62e331d98
Aiden Wilson <aiden_dw@icloud.com>
2024-09-23 23:44:12 +1000

    Main Menu

---
+++ MealPrepPlanner-XPlatform/View/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace MealPrepPlanner_XPlatform.View;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void Exit_OnClicked(object? sender, EventArgs e)
+    {
+        Application.Current?.Quit(); 
+    }
+
+
+    private void MealPlanner_OnClicked(object? sender, EventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void RecipeBook_OnClicked(object? sender, EventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+}
